a1236edd4043b3cba6aa3443713dd8dd0ebb38c2
Tanishq Sharma <123730739+TanishqSharma2022@users.noreply.github.com>
2023-06-11 14:44:54 +0530

    Updated the landing page
    Replaced

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ $(document).ready(function(){
         $("html, body").animate({
             scrollTop: $(
               'html, body').get(0).scrollHeight
-        }, 2000);
+        }, 800);
     });
 });
+$(document).ready(function(){
+    $('#aboutus_href').click(function(){
+        $("html, body").animate({
+            scrollTop: $(
+              'html, body').get(0).scrollHeight
+        }, 800);
+    });
+});
+const menu = document.querySelector(".list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = () => {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+}
+
+cancelBtn.onclick = () => {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+
+}
+
+var scrollTop = $(window).scrollTop() ;
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar")
+body.onscroll = (event) => {
+    // navbar.classList.add("whitener");
+    if(scrollTop > 100 + 'px'){
+        navbar.classList.add("whitener");
+    }    
+
+}
