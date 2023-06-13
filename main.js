98823093433e259ab16cea10796d0d5ab6aae2d8
Shailesh22290 <124439321+Shailesh22290@users.noreply.github.com>
2023-06-14 01:01:48 +0530

    Added changes

--- main.js
+++ main.js
@@ -39,3 +39,20 @@ body.onscroll = (event) => {
     }    
 
 }
+/////////////// scroll top ///////////////////////////
+var stepTime = 20;
+var docBody = document.body;
+var focElem = document.documentElement;
+var scrollAnimationStep = function (initPos, stepAmount) {
+    var newPos = initPos - stepAmount > 0 ? initPos - stepAmount : 0;
+    docBody.scrollTop = focElem.scrollTop = newPos;
+    newPos && setTimeout(function () {
+        scrollAnimationStep(newPos, stepAmount);
+    }, stepTime);
+}
+var scrollTopAnimated = function (speed) {
+    var topOffset = docBody.scrollTop || focElem.scrollTop;
+    var stepAmount = topOffset;
+    speed && (stepAmount = (topOffset * stepTime)/speed);
+    scrollAnimationStep(topOffset, stepAmount);
+};
