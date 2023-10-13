93ef1c45d3f8fb5b3ae310e39e520dccc397bb1c
Tanishq Sharma <123730739+TanishqSharma2022@users.noreply.github.com>
2023-10-13 16:48:54 +0530

    changed colors, navbar blur effect and the image modal

--- scripts/main.js
+++ scripts/main.js
@@ -68,7 +68,6 @@ function close_modal(){
             const modal_container = document.querySelector(".modal_container");
             
             modal.classList.toggle("appear");
-            console.log(image.src)
             // modal_container.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${String(image.src)})`;
             modal_image.src = image.src;
 
